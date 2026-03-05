539ec22f64f2a5c8aa195846148efa5368c009d5
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-03-05 14:57:00 +0100

    chore: dockerfile + dockercompose

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 const registerRoutes = require("./features/register");
 const loginRoutes = require("./features/login");
